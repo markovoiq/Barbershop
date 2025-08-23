44fd1948dc2bf91da0f806490ce5aca2ce97d0e1
rubyst <markovoiq@outlook.com>
2025-08-24 00:51:02 +0300

    create database on app init

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+configure do
+	@db = SQLite3::Database.new 'barbershop.db'
+	@db.execute 'CREATE TABLE IF NOT EXISTS
+	 "Users" (
+	 	"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+	 	"name" TEXT,
+	 	"phone" TEXT,
+	 	"datestamp" TEXT, 
+	 	"barber" TEXT, 
+	 	"color" TEXT);'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -22,7 +35,6 @@ post '/visit' do
 	@barber = params[:barber]
 	@color = params[:color]
 
-	# хеш
 	hh = { 	:username => 'Введите имя',
 			:phone => 'Введите телефон',
 			:datetime => 'Введите дату и время' }
